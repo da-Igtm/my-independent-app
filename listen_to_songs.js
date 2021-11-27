741410124c93e774a19b009ab9e168897f2a8be6
da-Igtm <85411715+da-Igtm@users.noreply.github.com>
2021-11-27 22:13:58 +0530

    Add files via upload

---
+++ listen_to_songs.js
@@ -0,0 +1,12 @@
+var x= document.getElementById("audio1");
+var y= document.getElementById("audio2");
+var z= document.getElementById("audio3.mp3");
+function playSong1(){
+x.play();
+}
+function playSong2(){
+y.play();    
+}
+function playSong3(){
+z.play();   
+} 
---
+++ main.js
@@ -0,0 +1,5 @@
+function openPage(){
+user_name=document.getElementById("user_name",user_name);
+localStorage.setItem("user_name").value;
+window.location="listen_to_songs.html";
+}
